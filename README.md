192520354d229a5dc3e0bd2b35c851638e3bb98e
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2016-04-23 12:55:24 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # processflac.sh
 A Bash script to process FLAC files such as removing metadata, compressing, adding replaygain and much more
+
+## Processing
+This script can do the following processing:
+
+- Replace metadata based on a whitelist
+- Removed ID3 tags from FLAC files (considered as a corrupt FLAC files if found)
+- Remove cover from FLAC files
+- Import existing cover to FLAC files (the cover file must be in the album's folder)
+- Compress cover file using jpegoptim
+- Export lyrics contained in FLAC to .srt files
+- Import lyrics to FLAC files
+- Delete lyrics from hard drive
+- Add comment to FLAC files
+- Compress FLAC files
+- Add ReplayGain to FLAC files
+- Move files to a destination folder
+- Delete unwanted files from folder (such as .cue, .nfo, and so)
+
+## Configuration
+Just open the script file and change the constants to process the files as you want
+
+Note: Your music collection must be structured as follow:
+
+    artists/
+    	albums/
+    		FLAC files
+    		cover file
+
+## More informations
+- The compress process is only made once. When a successfully compression is done, the script saves the current encoder used to process the file directly in the FLAC file. So you when you relaunch the script, it will only try to compress files that were not already compressed (useful when having a huge collection)
+
+## Dependencies
+This script depends on the following commands:
+
+- bash
+- flac
+- metaflac
+- jpegoptim
+- id3v2
+- sed
+- grep
+
+## Todo / known possible improvements
+- Export lyrics only if present in FLAC file
+- Add the possibility to have a whitelist of files to keep instead of modifing the `find` command
+- Change the structure for searching musics
+- Make a better documentation (supported metadata, and much more...)
+
+# Sources (unformated for the moment)
+The following sources were used to make this script. Thanks to them for the help !
+
+- http://www.hydrogenaud.io/forums/index.php?showtopic=84634
+- http://stackoverflow.com/questions/3810709/how-to-evaluate-a-boolean-variable-in-an-if-block-in-bash
+- http://stackoverflow.com/questions/9755068/bash-list-and-sort-files-and-their-sizes-and-by-name-and-size
+- http://stackoverflow.com/questions/9011233/for-files-in-directory-only-echo-filename-no-path
+- http://stackoverflow.com/questions/16365938/create-list-of-all-files-in-every-subdirectories-in-bash
+- http://www.xiph.org/vorbis/doc/v-comment.html
+- http://isrc.ifpi.org/en/
+- https://wiki.xiph.org/VorbisComment
+- http://age.hobba.nl/audio/mirroredpages/ogg-tagging.html
+- https://wiki.xiph.org/Field_names
+- http://www.legroom.net/2009/05/09/ogg-vorbis-and-flac-comment-field-recommendations
+- http://stackoverflow.com/questions/20243467/write-bash-array-to-file-with-newlines
+- http://www.unix.com/shell-programming-and-scripting/191217-using-whitelist-file-remove-entries.html
+- https://hydrogenaud.io/index.php/topic,61371.0.html
+- https://sourceforge.net/p/flac/feature-requests/98/
+- https://code.google.com/archive/p/libkate/wikis/CreatingKateStreams.wiki
+- http://unix.stackexchange.com/questions/153862/remove-all-files-directories-except-for-one-file
+- http://www.unix.com/shell-programming-and-scripting/191217-using-whitelist-file-remove-entries.html
+- http://stackoverflow.com/questions/7359527/removing-trailing-starting-newlines-with-sed-awk-tr-and-friends
+- https://hydrogenaud.io/index.php/topic,36283.0.html
+- http://stackoverflow.com/questions/2292847/how-to-silence-output-in-a-bash-script
+- http://www.davidpashley.com/articles/writing-robust-shell-scripts/
+- http://askubuntu.com/questions/385528/how-to-increment-a-variable-in-bash
+- http://stackoverflow.com/questions/17830326/ignoring-specific-errors-on-shell-script
+- http://superuser.com/questions/352289/bash-scripting-test-for-empty-directory
+- http://unix.stackexchange.com/questions/156534/bash-script-error-with-strings-with-paths-that-have-spaces-and-wildcards
+- http://www.bobulous.org.uk/misc/Replay-Gain-in-Linux.html
+- https://bash.cyberciti.biz/guide/Create_an_integer_variable
+- http://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
---
+++ processflac.sh
@@ -0,0 +1,298 @@
+#!/usr/bin/env bash
+######################################################
+# Name:				processflac.sh
+# Author:			Ludovic Delafontaine
+# Date:				29.06.2015 - 22.04.2016
+# Description:		Process FLACs files to add, edit or remove tags and/or compress the file and much more
+# Documentation:	https://github.com/ludelafo/processflac.sh
+######################################################
+
+######################################################
+# In case of problems, stop the script
+set -e
+set -u
+
+######################################################
+# Consts - Change here if needed
+######################################################
+REPLACE_METADATA=true
+REMOVE_COVER=true
+IMPORT_COVER=false
+COMPRESS_COVER_FILE=true
+DELETE_COVER_FILE=true
+EXPORT_LYRICS=true
+IMPORT_LYRICS=true
+DELETE_LYRICS_FILE=true
+ADD_COMMENT=false
+COMPRESS_FLAC_FILE=true
+ADD_REPLAYGAIN=true
+MOVE_FILES=false
+DELETE_UNWANTED_FILES=true # Only keeps FLACs, lyrics and cover.jpg by default
+
+COMMENT="Share with love <3"
+COVER_FILE="cover.jpg"
+ENCODER="$(flac --version) --compression-level-8"
+
+SOURCE_FOLDER="/home/ludelafo/Music/Temp"
+DESTINATION_FOLDER="/home/ludelafo/Music"
+
+TEMPLATE_METADATA="/tmp/template.txt"
+TEMP_METADATA="/tmp/metadata.txt"
+CLEAN_METADATA="/tmp/clean-metadata.txt"
+LOG_FILE="/tmp/workflac.logs"
+
+METADATA_TO_KEEP=(
+	ARTIST
+	ALBUM
+	MEDIA
+	DATE
+	TITLE
+	LANGUAGE
+	GENRE
+	TRACKNUMBER
+	TRACKTOTAL
+	DISCNUMBER
+	DISCTOTAL
+	ENCODER
+)
+
+######################################################
+# Functions
+######################################################
+verifyFlac() {
+	music="$1"
+	flac --warnings-as-errors --test --silent "$music"
+}
+
+removeCover() {
+	music="$1"
+	metaflac --remove --block-type=PICTURE "$music"
+}
+
+compressCover() {
+	coverFile="$1"
+	jpegoptim --quiet "$coverFile"
+}
+
+importCover() {
+	music="$1"
+	cover="$2"
+	metaflac --import-picture-from="$cover" "$music"
+}
+
+deleteCoverFile() {
+	coverFile="$1"
+	rm $coverFile
+}
+
+exportMetadata() {
+	music="$1"
+	exportTo="$2"
+	metaflac --export-tags-to="$exportTo" "$music"
+}
+
+cleanMetadata() {
+	metadata="$1"
+	templateMetadata="$2"
+	output="$3"
+	grep --word-regexp --file "$templateMetadata" "$metadata"  > "$output"
+}
+
+importMetadata() {
+	metadata="$1"
+	music="$2"
+	metaflac --remove-all-tags --import-tags-from="$metadata" "$music"
+}
+
+deleteID3Comments() {
+	music="$1"
+	id3v2 --delete-all "$music" &> /dev/null
+}
+
+exportLyrics() {
+	music="$1"
+	lyricsFile="$2"
+	metaflac --show-tag LYRICS "$music" > "$lyricsFile"
+}
+
+cleanLyrics() {
+	lyricsFile="$1"
+	sed -i -e 's/LYRICS=//g' "$lyricsFile"
+}
+
+importLyrics() {
+	lyricsFile="$1"
+	music="$2"
+	metaflac --set-tag-from-file="LYRICS=$lyricsFile" "$music"
+}
+
+deleteLyricsFile() {
+	lyricsFile="$1"
+	rm --force "$lyricsFile"
+}
+
+addComment() {
+	comment="$1"
+	music="$2"
+	metaflac --set-tag="COMMENT=$comment" "$music"
+}
+
+removeEncoder() {
+	music="$1"
+	metaflac --remove-tag ENCODER "$music"
+}
+
+compressFlac() {
+	music="$1"
+	flac --silent --delete-input-file --compression-level-8 --force --verify "$music"
+}
+
+addEncoder() {
+	encoder="$1"	
+	music="$2"
+	metaflac --set-tag="ENCODER=$encoder" "$music"
+}
+
+addReplayGain() {
+	album="$1"
+	metaflac --add-replay-gain "$album"/*.flac
+}
+
+deleteUnwantedFiles() {
+	directory="$1"
+	find $directory ! -name "*.flac" ! -name "$COVER_FILE" ! -name "*.srt" -type f -exec rm {} +
+}
+
+moveFiles() {
+	sourceDirectory="$1"
+	destinationDirectory="$2"
+	mkdir -p "$destinationDirectory"
+	mv "$sourceDirectory" "$destinationDirectory"
+}
+
+logs() {
+	message="$1"
+	echo "$message" >> "$LOG_FILE"
+}
+
+######################################################
+# Script
+######################################################
+
+# Enable "Internal Field Separateur" to allow filenames with spaces
+IFS=$'\n'
+
+rm --force "$LOG_FILE"
+
+for artist in $(find $SOURCE_FOLDER -mindepth 1 -maxdepth 1 -type d | sort); do
+
+	echo "${artist##*/}"
+
+	for album in $(find $artist -mindepth 1 -type d | sort); do
+
+		echo "  ${album##*/}"
+
+		if $REPLACE_METADATA; then
+			printf "%s\n" "${METADATA_TO_KEEP[@]}" > "$TEMPLATE_METADATA"
+		fi
+
+		if $COMPRESS_COVER_FILE && [[ -f "$album/$COVER_FILE" ]]; then
+			compressCover "$album/$COVER_FILE"
+		fi
+
+		declare -i errors=0
+
+		for music in $(find $album -type f -name "*.flac" | sort); do
+			
+			echo "    ${music##*/}"
+				
+			if [[ $(verifyFlac "$music" 2>&1) == "" ]]; then
+
+				if $REMOVE_COVER; then
+					removeCover "$music"
+				fi
+
+				if $IMPORT_COVER && [[ -f "$album/$COVER_FILE" ]]; then
+					importCover "$music" "$album/$COVER_FILE"
+				fi
+
+				if $EXPORT_LYRICS; then
+					exportLyrics "$music" "${music%.*}.srt"
+					cleanLyrics "${music%.*}.srt"
+				fi
+
+				if $REPLACE_METADATA; then
+
+					exportMetadata "$music" "$TEMP_METADATA"
+					cleanMetadata "$TEMP_METADATA" "$TEMPLATE_METADATA" "$CLEAN_METADATA"
+					deleteID3Comments "$music"
+					importMetadata "$CLEAN_METADATA" "$music"
+					
+					if $IMPORT_LYRICS && [[ -f "${music%.*}.srt" ]]; then
+						importLyrics "${music%.*}.srt" "$music"
+					fi
+
+				fi
+
+				if $DELETE_LYRICS_FILE; then
+					deleteLyricsFile "$album/${music##*/}.srt"
+				fi
+
+				if $ADD_COMMENT; then
+					addComment "$COMMENT" "$music"
+				fi
+
+				if $COMPRESS_FLAC_FILE && [[ "$(metaflac --show-tag ENCODER $music)" != "ENCODER=$ENCODER" ]]; then
+					removeEncoder "$music"
+
+					if [[ $(compressFlac "$music" 2>&1) == "" ]]; then
+						addEncoder "$ENCODER" "$music"
+					fi
+
+				fi
+				
+			else
+				echo "      The file has a problem. This will be logged..."
+				logs "The file '$music' was not succesfully verified."
+				errors=$((errors + 1))
+			fi
+
+		done
+		
+		if [[ $errors == 0 ]]; then
+
+			if $DELETE_COVER_FILE; then
+				deleteCoverFile "$album/$COVER_FILE"
+			fi
+
+			if $ADD_REPLAYGAIN; then
+				addReplayGain "$album"
+			fi
+			
+			if $DELETE_UNWANTED_FILES; then
+				deleteUnwantedFiles "$album"
+			fi
+
+			if $MOVE_FILES; then
+				moveFiles "$album" "$DESTINATION_FOLDER/${artist##*/}/"
+			fi
+
+			echo "  The album has been correctly processed."
+
+		fi
+
+	done
+
+	if [[ ! "$(ls --almost-all $artist)" ]]; then
+		rm -r "$artist"
+	fi
+
+done
+
+sync
+
+if [[ -f "$LOG_FILE" ]] && [[ -s $LOG_FILE ]]; then
+	echo "Some errors were found. Please check $LOG_FILE for more informations."
+fi
+
+echo "Everthing is done. Exiting program."
